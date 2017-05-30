50875367e7afbde728a75b56170991e5b3a75d50
Jack Le Hamster <jacklehamster@gmail.com>
2017-05-29 19:07:18 -0700

    Update gif.js

--- gif.js
+++ gif.js
@@ -1,3 +1,6 @@
+define(function() {
+
+
 // Generic functions
 var bitsToNum = function(ba) {
   return ba.reduce(function(s, n) { return s * 2 + n; }, 0);
@@ -349,3 +352,11 @@ if (typeof exports !== 'undefined') {
   exports.parseGIF = parseGIF;
 }
 // END_NON_BOOKMARKLET_CODE
+
+  
+  return {
+    parseGif: parseGif,
+    Stream: Stream,
+  };
+  
+});
